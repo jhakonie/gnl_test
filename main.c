2b79aec39be23c60fcc9b4cb7755cd078a6bf3b1
Johanna Hakoniemi <Johku@Johanna-Air.lan>
2020-08-16 15:55:04 +0300

    main with error tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jhakonie <jhakonie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 17:45:21 by jhakonie          #+#    #+#             */
-/*   Updated: 2020/08/05 23:28:20 by jhakonie         ###   ########.fr       */
+/*   Updated: 2020/08/16 15:45:08 by jhakonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
@@ -66,7 +66,101 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
+				printf("Line is: |%s|\n", line);
+			}
+			i++;
+		}
+	}
+	else if (ac == 3 && ft_strcmp(av[1], "error") == 0)
+	{
+		fd = open(av[2], O_RDONLY);
+		if (fd == -1)
+		{
+			printf("If you gave it a filename that isn't a file: open() failed and fd is -1\n");
+			printf("Then we try to call GNL with this fd = |%d|\n", fd);
+			while (x == 1 && i < 10)
+			{
+				x = get_next_line(fd, &line);
+				if (x == -1)
+					printf("GNL returns: |%d| and fd is: |%d|\n", x, fd);
+				else if (x == 1)
+				{
+					printf("Line is: |%s|\n", line);
+				}
+				else if (x == 0)
+				{
+					printf("Gnl returns |%d| Nothing left to read.\n", x);
+					printf("Line is: |%s|\n", line);
+				}
+				i++;
+			}
+		}
+		i = 0;
+		if (fd >= 0)
+		{
+			printf("If you gave it a real file name, it is opened and fd is: |%d|\n", fd);
+			printf("But then we close the fd |%d| and try to call GNL with the same fd\n", fd);
+			if (close(fd) == 0)
+				printf("closed fd: |%d|\n", fd);
+			while (x == 1 && i < 10)
+			{
+				x = get_next_line(fd, &line);
+				if (x == -1)
+					printf("error, Gnl returns |%d| and fd is |%d|\n", x, fd);
+				else if (x == 1)
+				{
+					printf("Line is: |%s|\n", line);
+				}
+				else if (x == 0)
+				{
+					printf("Gnl returns |%d| Nothing left to read.\n", x);
+					printf("Line is: |%s|\n", line);
+				}
+				i++;
+			}
+		}
+	}
+	else if (ac == 3 && ft_strcmp(av[1], "errorline") == 0)
+	{
+		fd = open(av[2], O_RDONLY);
+		printf("We will open the file given as second argument and then send fd |%d| and NULL (instead of &line) to GNL.\n", fd);
+		if (fd == -1)
+			printf("open() failed, try again with a valid file name.\n");
+		while (x == 1 && i < 10)
+		{
+			x = get_next_line(fd, NULL);
+			if (x == -1)
+				printf("Error, Gnl returns |%d| and fd is |%d|\n", x, fd);
+			else if (x == 1)
+			{
+				printf("Line is: |%s|\n", line);
+			}
+			else if (x == 0)
+			{
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
+				printf("Line is: |%s|\n", line);
+			}
+			i++;
+		}
+	}
+	else if (ac == 3 && ft_strcmp(av[1], "FD_SIZE") == 0)
+	{
+		fd = open(av[2], O_RDONLY);
+		printf("We will send fd = |%d| (given as second argument) to the file.\n", atoi(av[2]));
+		fd = atoi(av[2]);
+		while (x == 1 && i < 10)
+		{
+			x = get_next_line(fd, &line);
+			if (x == -1)
+				printf("Error, Gnl returns |%d| and fd is |%d|\n", x, fd);
+			else if (x == 1)
+			{
+				printf("Line is: |%s|\n", line);
+			}
+			else if (x == 0)
+			{
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
@@ -96,7 +190,7 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
@@ -115,7 +209,7 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
@@ -134,7 +228,7 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
@@ -154,19 +248,17 @@ int main(int ac, char **av)
 			}
 			else if (x == 0)
 			{
-				printf("Nothing left to read.\n");
+				printf("Gnl returns |%d| Nothing left to read.\n", x);
 				printf("Line is: |%s|\n", line);
 			}
 			i++;
 		}
 		printf("fd is %d\n", fd);
-
 	}
-	else
+	else if (ac > 4)
 	{
 		printf("wrong number of arguments, give 3 file names.\n");
 	}
-
 	// while (1)
 	// 	;
 
